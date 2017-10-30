568c50efe98a8da816a0a394b86279ea3bd07a19
talhoffer <talhoffer@users.noreply.github.com>
2017-10-30 16:14:44 +0100

    roughly fleshed out instrument types

--- src/Instrument.hs
+++ src/Instrument.hs
@@ -3,29 +3,67 @@
 module Instrument where
 
 import Data.Time (Day)
--- import YieldCurve
+import qualified Data.Map as Map
+import Data.Map (Map)
 
 data Currency = CHF | USD | EUR | CAD | JPY
     deriving (Eq, Show, Read)
 
--- data BondType = Fixed | Floating
+data CouponType = Fixed | Floating
+    deriving (Show)
 
-data FixedBond = FixedBond
-    { coupon :: Double
-    , frequency :: Double
-    , maturity :: Day
-    , issueDate :: Maybe Day
-    }
+newtype Coupon = Coupon Double
+    deriving (Show)
+    
+newtype ZSpread = ZSpread Double
+    deriving (Show)
 
-data FloatingBond = FloatingBond
-    { nextCoupon :: Maybe Double
-    , frequency :: Double
-    , maturity :: Day
+data Frequency = Annual | SemiAnnual | Quarterly
+    deriving (Eq, Ord, Show, Read)
+
+frequencyMap :: Map Frequency Double
+frequencyMap = Map.fromList
+    [ (Annual, 1.0)
+    , (SemiAnnual, 2.0)
+    , (Quarterly, 4.0)
+    ]
+
+data Maturity = Date Day | Perpetual
+    deriving (Eq, Show)
+
+data BondDef = BondDef
+    { couponType :: CouponType
+    , coupon :: Coupon
+    , frequency :: Frequency
+    , maturity :: Maturity
     , issueDate :: Maybe Day
-    }
+    , zSpread :: Double
+    } deriving (Show)
+
+newtype Exposure = Exposure Double
+
+data Instrument
+    = Bond BondDef
+    | BondFuture BondDef BondDef
+    | InterestRateSwap BondDef BondDef
+    | Composite [Instrument]
+
+-- bondFromRawData :: Bla -> Bond
+
+-- data FixedBond = FixedBond
+--     { coupon :: Double
+--     , frequency :: Double
+--     , maturity :: Day
+--     , issueDate :: Maybe Day
+--     }
+
+-- data FloatingBond = FloatingBond
+--     { nextCoupon :: Maybe Double
+--     , frequency :: Double
+--     , maturity :: Day
+--     , issueDate :: Maybe Day
+--     }
     
 -- data BondInput = ...
 
 -- data Bond = FixedBond | FloatingBond
-
--- type Instrument = [Bond]
