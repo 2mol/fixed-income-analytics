b0e2a540d92ee2f16ce7c64a34b618cabefd2e0e
talhoffer <talhoffer@users.noreply.github.com>
2017-11-19 03:37:12 +0100

    consolidation for cf calc

--- TODO.md
+++ TODO.md
@@ -3,7 +3,7 @@ TODO:
 
 - [ ] newtype, with derived Num instances instead of current 'type Foo = Double'.
 - [ ] function mkBond, so that Instrument module doesn not expose type constructors.
-- [ ] split last coupon and redemption (simplifies & is more flexible later).
+- [x] split last coupon and redemption (simplifies & is more flexible later).
 - [ ] think about going Date first, Term second. Might enable smart use of Fractional instead of Double (since daysDelta is always a multiplier for 1 / constYEARDAYS).
 - [ ] ieee754 float comparisons with Data.AEq (~==)
 - [x] doctest (http://yannesposito.com/Scratch/en/blog/Haskell-Tutorials--a-tutorial/)
@@ -18,4 +18,3 @@ Wishlist:
 - [ ] pull (& parse) yield curve data from various central bank sources or quantopian.
 - [ ] web tool
 - [ ] cashing
-
--- src/Instrument/CashFlow.hs
+++ src/Instrument/CashFlow.hs
@@ -3,7 +3,7 @@
 module Instrument.CashFlow where
 
 import Data.Time (Day, addDays, diffDays)
-import Data.List (zipWith4)
+-- import Data.List (zipWith4)
 import Instrument.Bond
     ( BondDef(..)
     , CouponDef(..)
@@ -11,46 +11,31 @@ import Instrument.Bond
 
 data CashFlow = CashFlow
     { term :: Double
-    , approxFlowDate :: Day
+    -- , approxFlowDate :: Day
     , nominalAmount :: Double
     , presentValue :: Double
-    }
+    } deriving Show
+
+data CashFlows = CashFlows Day Double [CashFlow] deriving Show
 
 constYEARDAYS :: Double
 constYEARDAYS = 365.24219
 
+calcFlowStream :: BondDef -> Day -> CashFlows
+calcFlowStream bond@BondDef{..} analysisDate =
+    CashFlows analysisDate principal $ calcFlows bond analysisDate
+
 calcFlows :: BondDef -> Day -> [CashFlow]
-calcFlows bond@BondDef{..} analysisDate =
+calcFlows
+    BondDef
+        { maturityDate = maturityDate
+        , frequency = frequency
+        , lastPaymentDate = lastPaymentDate
+        , mIssueDate = mIssueDate
+        , couponInfo = couponInfo
+        }
+    analysisDate =
     let
-        terms =
-            calcFlowTerms bond analysisDate
-
-        dates =
-            map (calcFlowDate analysisDate) terms
-
-        coupons =
-            case couponInfo of
-                Fixed coupon ->
-                    calcFixedCoupons coupon terms
-                Floating _ ->
-                    undefined
-
-        payments = map (*principal) coupons
-
-        presentValues =
-            repeat 0 -- TODO
-    in
-        zipWith4 CashFlow terms dates payments presentValues
-        -- CashFlow <$> terms <$> dates <$> payments <$> presentValues
-
--- helper functions:
-
-calcFlowTerms :: BondDef -> Day -> [Double]
-calcFlowTerms BondDef{..} analysisDate =
-    let
-        yearsBetweenFlows =
-            1 / frequency
-
         yearsToMaturity =
             dateDelta analysisDate maturityDate
 
@@ -66,24 +51,93 @@ calcFlowTerms BondDef{..} analysisDate =
         numberOfFlows =
             floor $ (yearsToLastPayment - firstPaymentLowerBound) * frequency
 
-        couponTerms =
+        yearsBetweenFlows =
+            1 / frequency
+
+        terms =
             scanl (-) yearsToLastPayment (replicate numberOfFlows yearsBetweenFlows)
+
+        coupons =
+            case couponInfo of
+                Fixed coupon ->
+                    calcFixedCoupons coupon terms
+                Floating _ ->
+                    undefined
+
+        presentValues =
+            repeat 667 -- TODO
+
+        couponPayments =
+            zipWith3 CashFlow terms coupons presentValues
+
+        principalRedemption =
+            CashFlow yearsToMaturity 1 666
     in
-        yearsToMaturity : couponTerms
+        principalRedemption : couponPayments
+
+-- calcFlowsOld :: BondDef -> Day -> [CashFlow]
+-- calcFlowsOld bond@BondDef{..} analysisDate =
+--     let
+--         terms =
+--             calcFlowTermsOld bond analysisDate
+
+--         coupons =
+--             case couponInfo of
+--                 Fixed coupon ->
+--                     calcFixedCoupons coupon terms
+--                 Floating _ ->
+--                     undefined
+
+--         payments = map (*principal) coupons
+
+--         presentValues =
+--             repeat 0 -- TODO
+--     in
+--         zipWith3 CashFlow terms payments presentValues
+        -- CashFlow <$> terms <$> dates <$> payments <$> presentValues
+
+-- helper functions:
+
+-- calcFlowTermsOld :: BondDef -> Day -> [Double]
+-- calcFlowTermsOld BondDef{..} analysisDate =
+--     let
+--         yearsBetweenFlows =
+--             1 / frequency
+
+--         yearsToMaturity =
+--             dateDelta analysisDate maturityDate
+
+--         yearsToLastPayment =
+--             dateDelta analysisDate lastPaymentDate
+
+--         firstPaymentLowerBound =
+--             case mIssueDate of
+--                 Nothing -> 0
+--                 Just issueDate ->
+--                     max (dateDelta analysisDate issueDate) 0
+
+--         numberOfFlows =
+--             floor $ (yearsToLastPayment - firstPaymentLowerBound) * frequency
+
+--         couponTerms =
+--             scanl (-) yearsToLastPayment (replicate numberOfFlows yearsBetweenFlows)
+--     in
+--         yearsToMaturity : couponTerms
 
 dateDelta :: Day -> Day -> Double
 dateDelta date1 date2 =
-    daysToYears $ diffDays date2 date1
+    -- dayDeltaToYearDelta $ diffDays date2 date1
+    fromIntegral (diffDays date2 date1) / constYEARDAYS
 
-daysToYears :: Integer -> Double
-daysToYears = (/constYEARDAYS) . fromIntegral
+-- dayDeltaToYearDelta :: Integer -> Double
+-- dayDeltaToYearDelta = (/constYEARDAYS) . fromIntegral
 
-calcFlowDate :: Day -> Double -> Day
-calcFlowDate analysisDate term =
-    let
-        daysDelta = round (term * constYEARDAYS)
-    in
-        addDays daysDelta analysisDate
+-- calcFlowDate :: Day -> Double -> Day
+-- calcFlowDate analysisDate term =
+--     let
+--         daysDelta = round (term * constYEARDAYS)
+--     in
+--         addDays daysDelta analysisDate
 
 calcFixedCoupons :: Double -> [a] -> [Double]
 calcFixedCoupons coupon terms = map (const coupon) terms ++ [1]
